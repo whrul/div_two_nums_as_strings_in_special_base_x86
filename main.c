3b350341ef632be40572c37b9bae5c5c3da7b2d9
whrul <valeragrul@gmail.com>
2020-01-02 21:02:06 +0100

    Work-version-1.0

--- main.c
+++ main.c
@@ -2,7 +2,9 @@
 #include <stdlib.h> // atoi, malloc
 #include <string.h> // strlen
 
+
 char *sdiv(unsigned int base, char *result, char *s1, char *s2); // remainder into s1
+char toLower(const char c);
 
 int main(int argc, char *argv[]) {
     if (argc < 4) {
@@ -10,8 +12,28 @@ int main(int argc, char *argv[]) {
         return 0;
     }
     int base = atoi(argv[1]);
-    if (base <= 0 || base > 16) { // for now only 10; also should check strings for illegal numbers also check s2 == 0 and s1<S2 ; in main check length > 0 should? from input cmd; remove 0 at the begining?
+    if (base <= 0 || base > 16) {
         printf("Wrong base format.\n"); 
+        return 0;
+    }
+    for (size_t i = 0; i < strlen(argv[2]); ++i) {
+        if (argv[2][i] >= '0' && argv[2][i] <= '9') {
+            continue;
+        }
+        if (toLower(argv[2][i]) >= 'a' && toLower(argv[2][i]) <= 'a' + base - 11) {
+            continue;
+        }
+        printf("Wrong s1 format.\n"); 
+        return 0;
+    }
+    for (size_t i = 0; i < strlen(argv[3]); ++i) {
+        if (argv[3][i] >= '0' && argv[3][i] <= '9') {
+            continue;
+        }
+        if (toLower(argv[3][i]) >= 'a' && toLower(argv[3][i]) <= 'a' + base - 11) {
+            continue;
+        }
+        printf("Wrong s2 format.\n"); 
         return 0;
     }
     size_t sizeInBytes = strlen(argv[2]) + 1;
@@ -20,10 +42,19 @@ int main(int argc, char *argv[]) {
         printf("Cannot alloc memory for result.\n");
         return 0;
     }
-    // argv[2] = "";
-    // argv[3] = "123123";     segmentation fault
     sdiv((unsigned int)base, result, argv[2], argv[3]);
     printf("Result: %s\nRemainder: %s\n\n", result, argv[2]);
     free(result);
     return 0;
 }
+
+char toLower(const char c) {
+    if (c >= 'A' && c <= 'Z') {
+        return c - 'A' + 'a';
+    }
+    return c;
+}
+
+// argv[2] = "";
+// argv[3] = "xxx";     segmentation fault
+
